be23ec3d05900b69a3d47bc1e0dd056c333125f4
Simon Willison <swillison@gmail.com>
2020-05-07 19:56:05 -0700

    Basic file serving, refs #2

--- datasette_media/app.py
+++ datasette_media/app.py
@@ -1,6 +1,7 @@
-from starlette.responses import HTMLResponse
+from starlette.responses import HTMLResponse, FileResponse
 from starlette.routing import Router, Route
 from starlette.endpoints import HTTPEndpoint
+from starlette.exceptions import HTTPException
 
 
 def serve_media_app(datasette):
@@ -11,11 +12,29 @@ def serve_media_app(datasette):
 
 
 def get_class(datasette):
+    plugin_config = datasette.plugin_config("datasette-media") or {}
+
     class ServeMedia(HTTPEndpoint):
         async def get(self, request):
             media_type = request.path_params["media_type"]
             key = request.path_params["key"]
-
-            return HTMLResponse("Type={}, Key={}".format(media_type, key))
+            config = plugin_config.get(media_type)
+            if config is None:
+                return HTMLResponse("<h1>Invalid media type</h1>", status_code=404)
+            sql = config.get("sql")
+            if sql is None:
+                return HTMLResponse(
+                    "<h1>Missing SQL from configuration</h1>", status_code=404
+                )
+            database = config.get("database")
+            if database is None:
+                database = next(iter(datasette.databases.keys()))
+            results = await datasette.execute(database, sql, {"key": key})
+            try:
+                row = results.rows[0]
+            except IndexError:
+                return HTMLResponse("<h1>404</h1>", status_code=404)
+            filepath = row["filepath"]
+            return FileResponse(filepath)
 
     return ServeMedia
--- tests/test_media.py
+++ tests/test_media.py
@@ -4,9 +4,22 @@ import httpx
 
 
 @pytest.mark.asyncio
-async def test_media():
-    app = Datasette([], memory=True).app()
+async def test_media_filepath(tmpdir):
+    filepath = tmpdir / "hello.txt"
+    filepath.write_text("hello", "utf-8")
+    app = Datasette(
+        [],
+        memory=True,
+        metadata={
+            "plugins": {
+                "datasette-media": {
+                    "photos": {"sql": "select '{}' as filepath".format(filepath)}
+                }
+            }
+        },
+    ).app()
     async with httpx.AsyncClient(app=app) as client:
-        response = await client.get("http://localhost/-/media/type/key")
+        response = await client.get("http://localhost/-/media/photos/key")
     assert 200 == response.status_code
-    assert "Type=type, Key=key" == response.content.decode("utf8")
+    assert "hello" == response.content.decode("utf8")
+    assert "text/plain; charset=utf-8" == response.headers["content-type"]
